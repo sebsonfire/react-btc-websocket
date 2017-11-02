346b8762d33034a67dd94b4da7f20958b0c26400
Sebastiaan van Kuyk <sebastiaan.vankuyk@gmail.com>
2017-11-02 03:44:06 +0100

    more styling

--- src/App.js
+++ src/App.js
@@ -9,9 +9,14 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <div className="text-center">
-          <h1> Send me BTC!</h1>
-          <TargetAccount />
+        <div className="text-center row">
+          <div className="col title">
+            <h1>MONITOR BITCOIN BLOCKCHAIN CONFIRMATIONS</h1>
+          </div>
+          <br />
+          <div>
+            <TargetAccount />
+          </div>
         </div>
       </div>
     );
